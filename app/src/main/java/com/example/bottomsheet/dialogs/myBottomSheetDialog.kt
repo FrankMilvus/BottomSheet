551ea51731e151784864040a53792deb269fa983
FrankMilvus <frankmilvus@gmail.com>
2025-07-24 12:17:33 +0300

    Initial commit

---
+++ app/src/main/java/com/example/bottomsheet/dialogs/myBottomSheetDialog.kt
@@ -0,0 +1,30 @@
+package com.example.bottomsheet.dialogs
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import com.example.bottomsheet.R
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class MyBottomSheetDialog : BottomSheetDialogFragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.bottom_sheet_layout,container,false)
+        val button1 : Button = view.findViewById(R.id.btn_1)
+        val button2 : Button = view.findViewById(R.id.btn_2)
+
+        button1.setOnClickListener {
+
+        }
+        button2.setOnClickListener {
+
+        }
+
+        return view
+    }
+}
